5be0645869a8915ebbccc9d2b4f510582e1eb762
RedoyRKS <redoyshaha06@gmail.com>
2023-11-23 20:41:44 +0600

    new

---
+++ Day9/app.js
@@ -0,0 +1,18 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i+". Hello Bangladesh");
+// }
+
+// const data = [12, 13 , 14];
+// for (let i=0; i<data.length; i++){
+//     console.log(data[i]*2);
+// }
+//or
+// const data = [12, 13, 14, 15 , "redoy"];
+// data.map((element, index) =>{
+//     console.log(element, index);
+// })
+
+// const data = [12, 13, 14 , "Saha"];
+// for(let x of data){
+//     console.log(x);
+// }
---
+++ Day9/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const data = [12, 13, 14, 15 , "redoy"];
+data.map((element, index) =>{
+    console.log(element, index);
+})

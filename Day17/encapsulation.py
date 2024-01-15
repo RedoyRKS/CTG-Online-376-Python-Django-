ea746510d9c35c0ef146182136f70f59e231c5ad
RedoyRKS <redoyshaha06@gmail.com>
2024-01-15 23:55:41 +0600

    new

---
+++ Day17/encapsulation.py
@@ -0,0 +1,15 @@
+class Student:
+    name = "Redoy"
+
+    # def __init__(self):
+    #     print(self.name)
+    def printDisplay(self):
+        print(self.name)
+
+
+setudentOne= Student()
+# Student.name="Anup"
+setudentOne.name= "Saha"
+
+setudentOne.printDisplay()
+# setudentTwo = Student()

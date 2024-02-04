73a6777dbf74247940142b4292e029be3182569e
RedoyRKS <redoyshaha06@gmail.com>
2024-02-04 20:50:25 +0600

    JS

--- Day10/Js/app.js
+++ Day10/Js/app.js
@@ -1,2 +1,11 @@
-const myPara = document.getElementById("para").innerText
-console.log(myPara)
+// const myPara = document.getElementById("para").innerText
+// console.log(myPara)
+
+const myBtn = document.getElementById("clickBtn");
+function gettingUserName(){
+    event.preventDefault();
+    const userFirstName = document.getElementById("username").value;
+    const userPassword = document.getElementById("userPassword").value;
+    console.log(userFirstName, userPassword);
+}
+myBtn = addEventListener("click", gettingUserName)
